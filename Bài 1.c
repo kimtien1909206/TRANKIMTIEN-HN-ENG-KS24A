1a2cf12748c3f99f40396332fde7066b1e3d2005
kimtien1909206 <hkimttien1236@gmail.com>
2024-11-20 14:29:11 +0700

    Bài 1 sesion 2

---
+++ Bài 1.c
@@ -0,0 +1,16 @@
+
+int main(){
+	// khai bao va luu tru cac kieu du lieu co ban
+	// kieu int: dung de luu tru so nguyen 
+	int age = 25; // khai bao bien age voi gia tri 25
+	// kieu float: dung de luu tru so thuc co phan thap phan
+	float hight = 1.75; // khai bao bien hight voi gia tri 1.75
+	// kieu double: tuong tu nhu float nhung co do chinh xac cao hon 
+	double weight = 70.5; // Khai báo bien weight voi giá tri 70.5
+    // Kieu char: dùng de luu tru mot ky tu
+    char initial = 'A'; // Khai bao bien initial voi giá tri 'A'
+    // Kieu bool (can thu vien stdbool.h): dung de luu tru gia tri true hoac false
+    #include <stdbool.h>
+    bool isStudent = true; // Khai báo bien isStudent voi giá tri true
+    return 0
+}

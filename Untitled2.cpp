ec7c5f877b81bf72eda55c95079efc689f96a939
kimtien1909206 <hkimttien1236@gmail.com>
2024-11-20 14:49:25 +0700

    Bai 2 session 2

---
+++ Untitled2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Khai báo và khoi tao hai bien so nguyên
+    int a = 11;
+    int b = 10;
+
+    // Tính tong cua a và b
+    int sum_result = a + b;
+
+    // In ket qua
+    cout << "Tong cua a và b là: " << sum_result << endl;
+
+    return 0;
+}
+
